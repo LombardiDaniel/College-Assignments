d5d3e886d1c4561f9a5e7cfc62a6fbbe4f7d2858
Daniel Lombardi <lombardi.daniel.o@gmail.com>
2022-08-18 16:48:39 -0300

    sla

--- Read-Write-Benchmark/headers/benchmarker.h
+++ Read-Write-Benchmark/headers/benchmarker.h
@@ -7,15 +7,16 @@
 #define MIN_SIZE                                                    2<<0
 #define MAX_SIZE                                                    2<<20
 
-// int main(void) {
-//   clock_t start_time = clock();
-//
-//   // code or function to benchmark
-//
-//   double elapsed_time = (double)(clock() - start_time) / CLOCKS_PER_SEC;
-//   printf("Done in %f seconds\n", elapsed_time);
-// }
+#define SEQUENTIAL                                                  0b00
+#define RANDOM                                                      0b01
 
+#define READ                                                        0b00
+#define WRITE                                                       0b10
+
+#define SEQUENTIAL_WRITE                              (SEQUENTIAL & READ)
+#define RANDOM_WRITE                                      (RANDOM & READ)
+#define SEQUENTIAL_READ                              (SEQUENTIAL & WRITE)
+#define RANDOM_READ                                      (RANDOM & WRITE)
 
 // #include <time.h>
 //
