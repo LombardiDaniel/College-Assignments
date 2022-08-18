dbc23dfafeed03016b551e79aae467c1702457de
eon-gg <eon.epqm@gmail.com>
2022-08-18 17:39:03 -0300

    Finishing main function

--- Read-Write-Benchmark/main.c
+++ Read-Write-Benchmark/main.c
@@ -4,29 +4,38 @@
 
 #define atoa(x) #x
 
-
-int main() {
-    int fileSize = -1;
-    scanf("%d", &fileSize);
-
-    char fileName[10];
-    FILE *file;
-
+FILE *createFile(int fileSize, char *fileName) {
     sprintf(fileName, "%d", fileSize);
     strcat(fileName, ".txt");
 
     // Open file
-    file = fopen(fileName, "wb");
+    return fopen(fileName, "wb");
+}
 
-    // Fill with n '@'s
+void fillFile(FILE *file, int fileSize) {
     for(int i=0; i<fileSize; i++) {
         fprintf(file, "@");
-    }
+    }    
+}
 
-    // Fill the struct
+int main() {
+    int fileSize = -1;
+    char fileName[10];
+
+    scanf("%d", &fileSize);
+    FILE *file = createFile(fileSize, fileName);
+    fillFile(file, fileSize);
+
+    // Fill the struct (BENCHMARK SEQUENTIAL READ)
     timedFileOp op;
     op.elapsedTime = -1;
+    op.type = SEQUENTIAL_READ;
+    op.fileSize = fileSize;
+    op.fileName = fileName;
 
+    benchmark(op);
+
+    printf("Sequencial Reading - Elapsed Time (s): %f\n", op.elapsedTime);
 
     return 0;
 }
