f16e4a20126ea726121c796e783e5a54e4874d14
eon-gg <eon.epqm@gmail.com>
2022-08-18 16:34:47 -0300

    Adding initial read example

--- Read-Write-Benchmark/benchmark.c
+++ Read-Write-Benchmark/benchmark.c
@@ -1,11 +1,35 @@
 #include <stdio.h>
 #include <time.h>
 
+void readFile(char *filePath) {
+    int fd, flags = 0;
+    char c;
+
+    fd = open(filePath, flags);
+
+    if(fd == -1) {
+        printf("Erro na abertura do arquivo: %s nao encontrado.\n", filePath);
+        return;
+    }
+
+    while(read(fd, &c, 1)) {
+        printf("%c", c);
+    }
+    printf("\n\n");
+
+    return;
+}
+
+void preadFile(FILE *file) {
+    return;
+}
+
 int main() {
 
     clock_t begin = clock();
 
-    printf("Test elapsed time: \n");
+    // Code goes here
+    readFile("foo.txt");
 
     clock_t end = clock();
     double elapsed_secs = (double) (end - begin) / CLOCKS_PER_SEC;
